9aff9b6ec779605cf1036b92eb884e1b2e4a94b0
Pavith Nimantha <pavithsky@gmail.com>
2024-03-31 04:06:13 +0530

    Navigation Started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
-
 import AddShop from './components/shops/AddShop';
 import AllShops from './components/shops/AllShops';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from './layout/Layout';
+import Navigator from './layout/Navigator';
+import GroundFloor from './components/navigator/GroundFloor';
+import FirstFloor from './components/navigator/FirstFloor';
+import SecondFloor from './components/navigator/SecondFloor';
+import ThirdFloor from './components/navigator/ThirdFloor';
+import SideBar from './components/navigator/SideBar';
 
 
 const router = createBrowserRouter([
@@ -10,7 +15,6 @@ const router = createBrowserRouter([
     path: "/",
     element: <Layout />,
 
-    
     children: [
       {
         index: true,
@@ -21,6 +25,37 @@ const router = createBrowserRouter([
         path: "add",
         element: <AddShop />,
       },
+
+      {  
+        path: "navigator",
+        element: <Navigator />,
+          
+        children: [
+
+          {
+            index: true,
+            element: <GroundFloor />,
+          },
+
+          {
+            path: "firstfloor",
+            element: <FirstFloor />,
+          },
+
+          {
+            path: "secondfloor",
+            element: <SecondFloor />,
+          },
+
+          {
+            path: "thirdfloor",
+            element: <ThirdFloor />,
+          },
+          
+      ]
+    }
+
+
 
     ],
   },
---
+++ frontend/src/components/navigator/SecondFloor.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SecondFloor = () => {
+  return (
+    <div>SecondFloor</div>
+  )
+}
+
+export default SecondFloor
